32b1d930a7816c796d222933f9c64a6d346bf7a5
ZISHEN WANG <64644075+zz11ss11zz@users.noreply.github.com>
2023-09-19 15:55:53 -0700

    Update exe_odd_tuples.py

--- Unit3/exe_odd_tuples.py
+++ Unit3/exe_odd_tuples.py
@@ -1,3 +1,6 @@
+# Write a procedure called oddTuples, which takes a tuple as input, and returns a new tuple as output, 
+# where every other element of the input tuple is copied, starting with the first one. 
+
 def oddTuples(aTup):
     '''
     aTup: a tuple
